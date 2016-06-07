d9a6b44649d18f4e38d0e1b5d182310387f2c90f
Ronan Forman <rf9@sanger.ac.uk>
2016-06-07 12:27:49 +0100

    Add put labware method and labware serializer

--- src/main/groovy/utils/RestService.groovy
+++ src/main/groovy/utils/RestService.groovy
@@ -4,18 +4,16 @@
 package utils
 
 import exceptions.RestServiceException
+import groovy.json.JsonSlurper
 import groovyx.net.http.HTTPBuilder
+
 import static groovyx.net.http.ContentType.JSON
 import static groovyx.net.http.Method.*
-
-import groovy.json.JsonOutput
-import groovy.json.JsonSlurper
-
 
 /**
  * The {@code RestService} class represents a utility class that can communicate
  * with REST services with GET/POST/UPDATE HTTP methods.
- * 
+ *
  * @author ke4
  *
  */
@@ -27,32 +25,14 @@ class RestService {
         http = new HTTPBuilder(serviceUrl)
     }
 
-    def post(path, payload) {
-        http.request( POST ) {
-            uri.path = path
-                requestContentType = JSON
-                body =  payload
-
-            response.success = { resp ->
-                resp.getEntity().getContent().text
-            }
-
-            response.failure = { resp ->
-                def slurper = new JsonSlurper()
-                def errors = slurper.parseText(resp.getEntity().getContent().text)
-                def errorMessage = errors.collect { key, value -> "${key.capitalize()} ${value.join(', ')}" }.join(".${System.getProperty('line.separator')}") 
-                throw new RestServiceException(errorMessage)
-            }
-        }
-    }
-
-    def get(path, queryParams) {
-        http.request(GET) {
-            uri.path = path
-            uri.query =  queryParams
+    private String request(Map params) {
+        http.request(params.requestType) {
+            uri.path = params.path
+            uri.query = params.query
             requestContentType = JSON
+            body = params.json
 
-            response.success = { resp ->
+            response.sucess = { resp ->
                 resp.getEntity().getContent().text
             }
 
@@ -63,5 +43,17 @@ class RestService {
                 throw new RestServiceException(errorMessage)
             }
         }
+    }
+
+    def post(path, payload) {
+        request(requestType: POST, path: path, json: payload)
+    }
+
+    def get(path, queryParams) {
+        request(requestType: GET, path: path, query: queryParams)
+    }
+
+    def put(path, payload) {
+        request(requestType: PUT, path: path, json: payload)
     }
 }
--- src/test/groovy/actions/LabwareActionsTest.groovy
+++ src/test/groovy/actions/LabwareActionsTest.groovy
@@ -3,13 +3,17 @@
  */
 package actions
 
+import models.Labware
+import models.LabwareType
+import models.Location
+import models.Receptacle
 import spock.lang.Specification
 import utils.RestService
 import utils.RestServiceConfig
 
 /**
  * A test class for labware creation.
- * 
+ *
  * @author ke4
  *
  */
@@ -25,7 +29,7 @@ class LabwareActionsTest extends Specification {
         LabwareActions.restService = restServiceStub
 
         when:
-        def targetPlate = LabwareActions.newPlate('generic 96 well plate', testExternalId, barcode: testBarcode)
+        def targetPlate = LabwareActions.newLabware('generic 96 well plate', testExternalId, barcode: testBarcode)
 
         then:
         targetPlate.externalId == testExternalId
@@ -38,13 +42,87 @@ class LabwareActionsTest extends Specification {
         def searchPlateByBarcodePath = RestServiceConfig.getLabwarePath()
 
         def restServiceStub = Stub(RestService)
-        restServiceStub.get(searchPlateByBarcodePath, [ barcode: sourcePlateBarcode ]) >> new File('./src/test/groovy/test_plate_with_materials.json').text
+        restServiceStub.get(searchPlateByBarcodePath, [barcode: sourcePlateBarcode]) >> new File('./src/test/groovy/test_plate_with_materials.json').text
         LabwareActions.restService = restServiceStub
 
         when:
-        def sourcePlate = LabwareActions.getPlateByBarcode(sourcePlateBarcode)
+        def sourcePlate = LabwareActions.getLabwareByBarcode(sourcePlateBarcode)
 
         then:
         sourcePlate.barcode == sourcePlateBarcode
+    }
+
+    def "perisiting a labware"() {
+        setup:
+        def labwareId = '1'
+        def labwareBarcode = 'TEST_123'
+        def labwareExternalId = '123456'
+        def labwareTypeName = 'two well plate'
+
+        def labware = new Labware(id: labwareId, barcode: labwareBarcode, externalId: labwareExternalId, labwareType: new LabwareType(name: labwareTypeName))
+        labware.receptacles = [new Receptacle(materialUuid: '123', location: new Location(name: 'A1')), new Receptacle(materialUuid: '456', location: new Location(name: 'A2'))]
+
+        def labwarePayload = [
+            data: [
+                id: labwareId,
+                attributes: [
+                    barcode: labwareBarcode,
+                    external_id: labwareExternalId
+                ],
+                relationships: [
+                    labware_type: [
+                        data: [
+                            attributes: [
+                                name: labwareTypeName
+                            ]
+                        ]
+                    ],
+                    receptacles: [
+                        data: [
+                            [
+                                attributes: [
+                                    material_uuid: '123'
+                                ],
+                                relationships: [
+                                    location: [
+                                        data: [
+                                            attributes: [
+                                                name: 'A1'
+                                            ]
+                                        ]
+                                    ]
+                                ]
+                            ],
+                            [
+                                attributes: [
+                                    material_uuid: '456'
+                                ],
+                                relationships: [
+                                    location: [
+                                        data: [
+                                            attributes: [
+                                                name: 'A2'
+                                            ]
+                                        ]
+                                    ]
+                                ]
+                            ]
+                        ]
+                    ]
+                ]
+            ]
+        ]
+
+        def restServiceStub = Stub(RestService)
+        restServiceStub.put(RestServiceConfig.labwarePath + labwareId, labwarePayload) >> new File('./src/test/groovy/test_plate_with_updated_materials.json').text
+        LabwareActions.restService = restServiceStub
+
+        when:
+        labware = LabwareActions.updateLabware(labware)
+
+        then:
+        labware.externalId == labwareExternalId
+        labware.barcode == labwareBarcode
+        labware.receptacles*.materialUuid as Set == ['123', '456'] as Set
     }
 }
